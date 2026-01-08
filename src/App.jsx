05e3788fe2b56a1a889d352ffeebccb715590f8c
lingeswaran <717823s132@kce.ac.in>
2026-01-08 10:15:24 +0530

    commit Customer Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
-
+import Home from './home/Home';
+import Customer from './customer/Customer'
+import CustomerNav from './customer/Dashboard';
 function App() {
 
   return (
     <>
-     
+      {/* <Home/> */}
+      <Customer/>
     </>
   )
 }
