e5d0f18f6924bf27fc25838be6bc2db283ad82c6
lingeswaran05 <160377031+lingeswaran05@users.noreply.github.com>
2026-01-27 14:56:03 +0000

    commit from Cherubin

--- shipfast/src/App.jsx
+++ shipfast/src/App.jsx
@@ -23,6 +23,7 @@ export default function App() {
           path="/"
           element={
             <> 
+            
                <Hero />
               <HeroBottom />
               <Features />
