44ddfae0cbbca7912c34fa6473e206ff6b64c970
lingeswaran <717823s132@kce.ac.in>
2026-05-23 15:24:38 +0530

    recommit

--- SHIPFAST/shipment/src/main/java/com/shipfast/shipment/repository/PricingSlabRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.shipfast.shipment.repository;
-//
-//import com.shipfast.shipment.entity.PricingSlab;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//
-//public interface PricingSlabRepository extends MongoRepository<PricingSlab, String> {
-//}
